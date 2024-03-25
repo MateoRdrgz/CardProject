31c9b076f564ca208b018dfd0cfbbc9a08930b3f
BlackArrowEtu <nathan.miconi@etu.univ-lyon1.fr>
2024-03-25 11:41:46 +0100

    récupération develop

--- Card/Card.java
+++ Card/Card.java
@@ -3,4 +3,7 @@ package Card;
 public abstract class Card {
     String cardName;
 
+    public Card(String cardName) {
+        this.cardName = cardName;
+    }
 }
--- Card/Pokemon/PokemonCard.java
+++ Card/Pokemon/PokemonCard.java
@@ -1,14 +1,47 @@
 package Card.Pokemon;
 
 import Card.Card;
+import jdk.jfr.Unsigned;
 
 import java.util.ArrayList;
 
 public class PokemonCard extends Card {
     PokemonTypes pokemonType;
     ArrayList<PokemonAttack> attacks;
+    Integer healthPoint;
 
-    public PokemonCard(PokemonTypes pokemonType, String name) {
+    public PokemonCard(PokemonTypes pokemonType, String name, PokemonAttack baseAttack, Integer healthPoint) {
+        super(name);
         this.pokemonType = pokemonType;
+        attacks.add(baseAttack);
+        this.healthPoint = healthPoint;
+    }
+
+    public void addNewAttack(PokemonAttack attack){
+        attacks.add(attack);
+    }
+
+    public PokemonTypes getPokemonType() {
+        return pokemonType;
+    }
+
+    public void setPokemonType(PokemonTypes pokemonType) {
+        this.pokemonType = pokemonType;
+    }
+
+    public ArrayList<PokemonAttack> getAttacks() {
+        return attacks;
+    }
+
+    public void setAttacks(ArrayList<PokemonAttack> attacks) {
+        this.attacks = attacks;
+    }
+
+    public Integer getHealthPoint() {
+        return healthPoint;
+    }
+
+    public void setHealthPoint(Integer healthPoint) {
+        this.healthPoint = healthPoint;
     }
 }
